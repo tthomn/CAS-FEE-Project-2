6087909b8530cdffb96ea212c15b8bf11046f62e
tthoman <thomann.tetiana@gmail.com>
2025-01-25 23:30:58 +0100

    Added Firebase cartClean-up

--- frontend/functions/src/index.ts
+++ frontend/functions/src/index.ts
@@ -1,17 +1,48 @@
-import * as functions from 'firebase-functions/v2';
-import admin from 'firebase-admin';
+import * as functions from "firebase-functions/v2";
+import admin from "firebase-admin";
 
 if (!admin.apps.length) {
   admin.initializeApp();
 }
 const db = admin.firestore();
 
+// Deletes all cart items older than 14 days AND with a guestId
+export const cartCollectionCleaner = functions.scheduler.onSchedule(
+  {
+    schedule: "every 336 hours", // 14 days in hours (14 * 24)
+    timeZone: "Europe/Zurich",
+  },
+  async (event) => {
+    try {
+      const carts = await db.collection("cart")
+        .where("guestId", "!=", null)
+        .get();
+
+      const batch = db.batch();
+      const now = Date.now();
+      const cutoff = now - 14 * 24 * 60 * 60 * 1000; // 14 days in milliseconds
+
+      carts.forEach((doc) => {
+        const addedAt = doc.data().addedAt?.toMillis();
+        if (addedAt && addedAt < cutoff) {
+          batch.delete(doc.ref);
+        }
+      });
+
+      await batch.commit();
+      console.log("CartItem older than 14 days have been deleted successfully.");
+    } catch (error) {
+      console.error("Error during cart cleanup:", error);
+    }
+  }
+);
+
 export const setAdditionalUserData = functions.https.onCall(async (context) => {
   // Ensure the request is authenticated
   if (!context.auth) {
     throw new functions.https.HttpsError(
-      'unauthenticated',
-      'Only authenticated users can set additional data.',
+      "unauthenticated",
+      "Only authenticated users can set additional data.",
     );
   }
   const {
@@ -30,14 +61,14 @@ export const setAdditionalUserData = functions.https.onCall(async (context) => {
 
   if (context.auth.uid !== uid) {
     throw new functions.https.HttpsError(
-      'permission-denied',
-      'You can only modify your own data.',
+      "permission-denied",
+      "You can only modify your own data.",
     );
   }
 
   try {
     // Update the Firestore user document
-    await db.collection('users').doc(uid).set({
+    await db.collection("users").doc(uid).set({
       title,
       name,
       surname,
@@ -46,18 +77,18 @@ export const setAdditionalUserData = functions.https.onCall(async (context) => {
       houseNumber,
       zip,
       city,
-      authType: 'user',
+      authType: "user",
       country,
       addedAt: admin.firestore.FieldValue.serverTimestamp(),
       email,
     });
 
-    return { message: 'Additional user data set successfully.' };
+    return {message: "Additional user data set successfully."};
   } catch (error) {
-    console.error('Error setting additional user data:', error);
+    console.error("Error setting additional user data:", error);
     throw new functions.https.HttpsError(
-      'internal',
-      'Failed to set additional user data.',
+      "internal",
+      "Failed to set additional user data.",
     );
   }
 });
@@ -65,32 +96,32 @@ export const setAdditionalUserData = functions.https.onCall(async (context) => {
 export const setAdmin = functions.https.onCall(async (request) => {
   try {
     const uid = request.auth?.uid;
-    let authType = '';
+    let authType = "";
 
     if (!uid) {
-      throw new Error('User is not authenticated');
+      throw new Error("User is not authenticated");
     }
     await db
-      .collection('users')
+      .collection("users")
       .doc(uid)
       .get()
       .then((doc) => {
         if (doc.exists) {
           authType = doc.data()?.authType;
         } else {
-          authType = 'user';
+          authType = "user";
         }
       });
 
-    if (authType !== 'admin') {
-      return { message: `User ${uid} is not allowed to be admin` };
+    if (authType !== "admin") {
+      return {message: `User ${uid} is not allowed to be admin`};
     } else {
-      admin.auth().setCustomUserClaims(uid, { admin: true });
+      admin.auth().setCustomUserClaims(uid, {admin: true});
 
-      return { message: `User ${uid} is now an admin.` };
+      return {message: `User ${uid} is now an admin.`};
     }
   } catch (error) {
-    console.error('Error setting admin role:', error);
-    return { message: 'Error setting admin role.' };
+    console.error("Error setting admin role:", error);
+    return {message: "Error setting admin role."};
   }
 });
